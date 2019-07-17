8ad63f96a519e75345eefd871c521c0fb9bf2dbb
leiyang <568681002@qq.com>
2019-07-17 14:07:11 +0800

    第二次提交

---
+++ style.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.11 : Database - jeecg-boot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jeecg-boot` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `jeecg-boot`;
+
+/*Table structure for table `sys_user_style` */
+
+DROP TABLE IF EXISTS `sys_user_style`;
+
+CREATE TABLE `sys_user_style` (
+  `id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
+  `create_by` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT NULL COMMENT '创建日期',
+  `update_by` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '更新人',
+  `update_time` datetime DEFAULT NULL COMMENT '更新日期',
+  `sys_org_code` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '所属部门',
+  `username` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '登录账号',
+  `theme_setting` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '风格',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `sys_user_style` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
